2738d4013a6f567c06e89b260f569abd1bfcebbb
Ryan Shankar <shankary@oregonstate.edu>
2025-05-15 09:43:26 -0700

    Recursion in GO 😩

--- forloops.go
+++ forloops.go
@@ -10,3 +10,21 @@ func runforloop() {
 	}
 	fmt.Println(sum)
 }
+
+func runwhileloop() {
+	counter := 0
+	sum := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		sum += counter
+		counter += 1
+	}
+	fmt.Println(sum)
+}
+
+func recursion(n int) int {
+	if n == 0 {
+		return 0
+	}
+	return 1 + recursion(n-1)
+}
--- helloworld.go
+++ helloworld.go
@@ -1,6 +1,10 @@
 package main
 
 import "fmt"
-func main(){
+
+func main() {
 	fmt.Println("Hello World, Im learning go on May 14th 2025")
+	runforloop()
+	runwhileloop()
+	fmt.Println(recursion(5))
 }
